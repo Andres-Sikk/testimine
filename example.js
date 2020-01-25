0701f60338858f71c60f2089d73aebe03683ee7e
Andres-Sikk <andres.sikk@tptlive.ee>
2020-01-25 13:32:40 +0200

    Add files

---
+++ example.js
@@ -0,0 +1,28 @@
+const a = 3;
+let b = 1;
+var c = 2;
+/*
+console.log(a, b, c);
+b = 3363;
+console.log({a, b});
+if(a>b){
+console.log("a on suurem kui b")
+}
+else{
+    console.log("b on suurem või võrdne kui a")
+}
+for(let i = 0; i<5; i++){
+    console.log("aaa", i)
+}
+function sum(param1, param2 = 5){
+    return param1 + param2;
+}
+console.log("sum(1,10)=", sum(1,10))
+console.log("sum(1)=", sum(1))//default
+const sum2 = sum(param1, param2 = 5) => {
+    return param1 + param2;
+}
+console.log("sum2(1,10)=", sum2(1,10))
+console.log("sum2(1)=", sum2(1))//default
+*/
+//ära kasuta for array loopimiseks, kasuta arr.forEach
